11b9637d4b2498cfa8c65e87fdbbabdc8c148355
Dilip Kumar <41532429+dilipkr7587@users.noreply.github.com>
2018-07-29 21:56:48 +0530

    test

--- src/test/Counter.test.js
+++ src/test/Counter.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from '../container/Counter';
 import configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux';
 
 const initialState = {}; 
 const mockStore = configureStore();
@@ -9,6 +10,8 @@ const mockStore = configureStore();
 it('renders without crashing', () => {
   const store = mockStore(initialState)
   const div = document.createElement('div');
-  ReactDOM.render(<Counter store={store}/>, div);
+  ReactDOM.render(<Provider store={store}>
+         <Counter />
+       </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
